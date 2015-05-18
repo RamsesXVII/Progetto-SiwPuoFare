e4b7a9f9013b31467f74760e2307c31de0236baa
RamsesXVII <mattia.iodice3@gmail.com>
2015-05-18 19:44:30 +0200

    modifico customerfacade

--- src/it/uniroma3/model/CommerceMain.java
+++ src/it/uniroma3/model/CommerceMain.java
@@ -6,8 +6,7 @@ import javax.persistence.*;
 public class CommerceMain {
 	
 	public static void main(String[] args) {
-		ProductFacade pf= new ProductFacade();
-		System.out.println(pf.getProduct(new Long(4351)).getName());
-
+		 ProductFacade pf= new ProductFacade();
+		 System.out.println(pf.getProduct(new Long(4351)).getName());
 	}
 }
