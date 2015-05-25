00d5372f98bc9cec7c9244133561ed372334ad0a
RamsesXVII <mattia.iodice3@gmail.com>
2015-05-25 14:49:41 +0200

    aggiungo caso d'uso1 con jsf

---
+++ src/it/uniroma3/controller/CustomerController.java
@@ -0,0 +1,189 @@
+package it.uniroma3.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import it.uniroma3.model.Address;
+import it.uniroma3.model.Customer;
+import it.uniroma3.model.CustomerFacade;
+
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+
+@ManagedBean
+public class CustomerController {
+	
+	@ManagedProperty(value="#{param.id}")
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String dateOfBirth;
+	private Date registrationDate;
+	private Address address;
+	private List<Customer> customers;
+	private Customer customer;
+	private String street;
+	private String city;
+	private String state;
+	private String zipcode;
+	private String country;
+	
+	@EJB
+	private CustomerFacade customerFacade;
+	
+	public String createCustomer(){
+		this.customer=customerFacade.createCustomer(firstName, lastName, email, password, dateOfBirth, street, city, state, zipcode, country);
+		return "customer";
+	}
+	
+	public String listCustomers() {
+		this.customers = customerFacade.getAllCustomer();
+		return "customers"; 
+	}
+
+	public String findCustomer() {
+		this.customer = customerFacade.getCustomer(id);
+		return "customer";
+	}
+	
+	public String findCustomer(Long id) {
+		this.customer = customerFacade.getCustomer(id);
+		return "customer";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public CustomerFacade getCustomerFacade() {
+		return customerFacade;
+	}
+
+	public void setCustomerFacade(CustomerFacade customerFacade) {
+		this.customerFacade = customerFacade;
+	}
+
+}
+
+
